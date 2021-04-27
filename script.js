27e17de1e03d4c65a9e72b30aebf49943e3b6344
Heggy Castaneda <heggyy@gmail.com>
2021-04-27 15:53:20 -0400

    finish data is coming from mock

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ const renderCard = data => {
   // console.log('data called from renderCard!', data);
   const cards = document.getElementById('cards');
   const someArr = data.map(item => {
-    cards.innerHTML = `
+    const card = document.createElement('div');
+    card.innerHTML = `
       <div class="movie">
         <div class="card" style="width: 18rem;">
           <img src="${item.imageURL}" class="card-img-top" alt="The Dark Knight">
           <div class="card-body">
             <span class="badge badge-secondary">${item.date}</span>
-            <h5 class="card-title">${item.imageURL}</h5>
+            <h5 class="card-title">${item.title}</h5>
             <button class="btn btn-primary add-movie" onclick="saveToWatchlist(tt0468569)">Add
           </button></div>
         </div>
       </div>
     `;
+    cards.appendChild(card);
   });
 }
 const fetchData = url => {
