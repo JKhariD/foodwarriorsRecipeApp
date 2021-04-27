015b39da32cd3b475ae8c3b7e871dc475e6910a1
Heggy Castaneda <heggyy@gmail.com>
2021-04-27 15:44:47 -0400

    add cards class for main

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-alert('Hey');
+// alert('Hey');
+const renderCard = data => {
+  // console.log('data called from renderCard!', data);
+  const cards = document.getElementById('cards');
+  cons someArr = data.map( item => {
+    cards.innerHTML = `
+      <div class="movie">
+        <div class="card" style="width: 18rem;">
+          <img src="${item.imageURL}" class="card-img-top" alt="The Dark Knight">
+          <div class="card-body">
+            <span class="badge badge-secondary">${item.date}</span>
+            <h5 class="card-title">${item.imageURL}</h5>
+            <button class="btn btn-primary add-movie" onclick="saveToWatchlist(tt0468569)">Add
+          </button></div>
+        </div>
+      </div>
+    `;
+  });
+}
+const fetchData = url => {
+  axios.get(url)
+  .then(response => {
+    // console.log("axios response data", response.data);
+    renderCard(response.data);
+  })
+  .catch(err => console.error(err));
+}
 
 const button = document.getElementById('button');
 button.addEventListener('click', () => {
-  alert('hey clicked!');
+  fetchData('./mock/data.json');
 })
