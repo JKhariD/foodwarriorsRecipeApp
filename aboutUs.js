9e99b60c8b2c911ed2f8a64850d5187d4a54404b
Heggy Castaneda <heggyy@gmail.com>
2021-05-03 02:38:55 -0400

    add ios home screen icons and fix markup

--- aboutUs.js
+++ aboutUs.js
@@ -3,13 +3,14 @@ const results = document.getElementById("result");
 const renderFolks = data => {
     const resultHTML = data.map(item => {
         return `
-            <div class="portrait"
+            <div class="portrait">
                 <div class="card">
-                    <img src="${item.Face}" class="card-img-top" alt="...">
+                    <img src="${item.Face}" class="card-img-top" alt="${item.Name}">
                     <div class="card-body">
-                    <h5 class="card-title">${item.Name}</h5>
-                    <p class="card-text">${item.About}</p>
-                    <a href="${item.Linkedin}" class="btn btn-primary">Linkedin</a>
+                        <h5 class="card-title">${item.Name}</h5>
+                        <p class="card-text">${item.About}</p>
+                        <a href="${item.Linkedin}" class="btn btn-primary">Linkedin</a>
+                    </div>
                 </div>
             </div>
         `;
